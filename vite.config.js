ee0974a80ead776ae968399ebf1b9713b6e5aeba
derui <derutakayu@gmail.com>
2022-07-28 08:17:19 +0900

    fix: use alias instead of tsconfig

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
 const isProduction = process.env['NODE_ENV'] === 'production';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
   },
   publicDir: 'dist',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });

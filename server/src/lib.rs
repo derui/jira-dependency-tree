82120816b7a77687127a697570d7fb1a5d7e909c
derui <derutakayu@gmail.com>
2022-08-23 22:31:56 +0900

    feat: add route to load project

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -32,11 +32,31 @@ pub async fn handler(event: Request) -> Result<Response<Body>, Error> {
             &Method::POST => execute_load_issue(&event).await,
             _ => unmatch,
         },
+        "/load-project" => match event.method() {
+            &Method::POST => execute_load_project(&event).await,
+            _ => unmatch,
+        },
         _ => unmatch,
     }
 }
 
 async fn execute_load_issue(event: &Request) -> Result<Response<Body>, Error> {
+    let _json: IssueLoadingRequest = match event.body() {
+        Body::Text(text) => serde_json::from_str(text).map_err(|_| "Invalid format"),
+        _ => Err("Invalid body type"),
+    }?;
+
+    // Return something that implements IntoResponse.
+    // It will be serialized to the right response event automatically by the runtime
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body("Hello AWS Lambda HTTP request".into())
+        .map_err(Box::new)?;
+    Ok(resp)
+}
+
+async fn execute_load_project(event: &Request) -> Result<Response<Body>, Error> {
     let _json: IssueLoadingRequest = match event.body() {
         Body::Text(text) => serde_json::from_str(text).map_err(|_| "Invalid format"),
         _ => Err("Invalid body type"),

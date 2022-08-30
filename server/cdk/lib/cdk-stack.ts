a8abfeeb8f30c36d57d478b3141cd7ecdbf38ff5
derui <derutakayu@gmail.com>
2022-08-30 22:22:24 +0900

    feat: add stacks to deploy lambda and REST API

--- server/cdk/lib/cdk-stack.ts
+++ server/cdk/lib/cdk-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const jiraIssueHandlerLambda = new lambda.DockerImageFunction(this, "jiraIssueLoader", {
+      functionName: "jira-issue-loader",
+      code: lambda.DockerImageCode.fromImageAsset("../"),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: jiraIssueHandlerLambda,
+    });
   }
 }

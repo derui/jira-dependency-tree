72e82d257e527deea36bee4261870ca4abba5e60
derui <derutakayu@gmail.com>
2022-09-10 09:11:19 +0900

    feat: add zoom slider

---
+++ src/components/zoom-slider.tsx
@@ -0,0 +1,45 @@
+import { jsx } from "snabbdom"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import { MemoryStream } from "xstream";
+import { ComponentSinks, ComponentSources } from "@/components/type";
+import { selectAsMain } from "./helper";
+
+export interface ZoomSliderProps {
+  zoom: number;
+}
+
+type ZoomSliderSources = ComponentSources<{
+  props: MemoryStream<ZoomSliderProps>;
+}>;
+
+type ZoomSliderSinks = ComponentSinks<{}>;
+
+const intent = function intent(sources: ZoomSliderSources) {
+  const clicked$ = selectAsMain(sources, ".zoom-slider").events("click").mapTo(true);
+
+  return { props$: sources.props, clicked$ };
+};
+
+const model = function model(actions: ReturnType<typeof intent>) {
+  const zoom$ = actions.props$.map((v) => v.zoom);
+
+  return zoom$;
+};
+
+const view = function view(state$: ReturnType<typeof model>) {
+  return state$.map((zoom) => {
+    return (
+      <div class={{ "zoom-slider": true }} dataset={{ testid: "main" }}>
+        <span class={{ "zoom-slider__current-zoom": true }} dataset={{ testid: "current-zoom" }}>{`${zoom}%`}</span>
+      </div>
+    );
+  });
+};
+
+export const ZoomSlider = function ZoomSlider(sources: ZoomSliderSources): ZoomSliderSinks {
+  const actions = intent(sources);
+  const state$ = model(actions);
+
+  return {
+    DOM: view(state$),
+  };
+};
---
+++ test/components/zoom-slider.test.ts
@@ -0,0 +1,43 @@
+import { ZoomSlider } from "@/components/zoom-slider";
+import { mockDOMSource, VNode } from "@cycle/dom";
+import { mockTimeSource } from "@cycle/time";
+import { select } from "snabbdom-selector";
+import { suite } from "uvu";
+
+const test = suite("components/ZoomSlider");
+
+test("display current zoom value", async () => {
+  await new Promise<void>(async (resolve, rej) => {
+    // Arrange
+    const Time = mockTimeSource();
+    const dom = mockDOMSource({});
+
+    const sinks = ZoomSlider({
+      DOM: dom as any,
+      props: Time.diagram("-x", { x: { zoom: 50 } }),
+    });
+
+    // Act
+    const actual$ = sinks.DOM.map((vtree) => {
+      return {
+        zoom: select("[data-testid=current-zoom]", vtree)[0].text,
+      };
+    });
+
+    // Assert
+    const expected$ = Time.diagram("-a-", {
+      a: {
+        zoom: "50%",
+      },
+    });
+
+    Time.assertEqual(actual$, expected$);
+
+    Time.run((e) => {
+      if (e) rej(e);
+      else resolve();
+    });
+  });
+});
+
+test.run();

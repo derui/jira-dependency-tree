22996dcfe4a561809a90e6da764f6e7e6fc5b8a4
derui <derutakayu@gmail.com>
2022-09-18 13:18:49 +0900

    chore: do not default proxy configuration

--- server/cdk/lib/cdk-stack.ts
+++ server/cdk/lib/cdk-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 
+declare const integration: apigw.LambdaIntegration;
+
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -15,10 +17,26 @@ export class CdkStack extends cdk.Stack {
     const restApi = new apigw.LambdaRestApi(this, "Endpoint", {
       handler: jiraIssueHandlerLambda,
       apiKeySourceType: apigw.ApiKeySourceType.HEADER,
+      proxy: false,
     });
 
-    const apiKey = new apigw.RateLimitedApiKey(this, "default", {
+    const integration = new apigw.LambdaIntegration(jiraIssueHandlerLambda, {
+      proxy: true,
+    });
+
+    const issues = restApi.root.addResource("load-issues");
+    issues.addMethod("POST", integration, { apiKeyRequired: true });
+    const project = restApi.root.addResource("load-project");
+    project.addMethod("POST", integration, { apiKeyRequired: true });
+
+    new apigw.RateLimitedApiKey(this, "default", {
       apiKeyName: "default",
+      apiStages: [
+        {
+          api: restApi,
+          stage: restApi.deploymentStage,
+        },
+      ],
       resources: [restApi],
       enabled: true,
       throttle: {

2b26f5a0f40e47f8f5b4acf2b6c7c924e1ff0a02
derui <derutakayu@gmail.com>
2022-12-03 10:23:13 +0900

    test: add case

---
+++ cypress/e2e/3_project-information.cy.ts
@@ -0,0 +1,19 @@
+describe("project-information", () => {
+  it("open editor and input project key", () => {
+    cy.visit("/");
+
+    // open editor
+    cy.testid("project-information/name").click();
+    cy.testid("project-information/editor").should("have.class", "--show");
+
+    // assert input
+    cy.testid("project-information/name-input").should("have.attr", "placeholder").and("include", "required");
+    cy.testid("project-information/name-input").type("KEY").should("have.value", "KEY");
+    cy.testid("project-information/submit").click();
+
+    // Apply and check after.
+    cy.testid("project-information/editor").should("not.have.class", "--show");
+    cy.testid("project-information/name").should("contain.text", "Click here");
+    cy.testid("project-information/marker").should("have.class", "--show");
+  });
+});

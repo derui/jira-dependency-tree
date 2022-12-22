8139c5fd56ff8a949728c9f6cc26f4c8db075617
derui <derutakayu@gmail.com>
2022-12-22 23:55:26 +0900

    fix: remove unused loader from integration loader

--- src/components/jira-loader.ts
+++ src/components/jira-loader.ts
@@ -2,17 +2,14 @@ import isolate from "@cycle/isolate";
 import { Reducer } from "@cycle/state";
 import xs, { Stream } from "xstream";
 import { JiraIssueLoader } from "./jira-issue-loader";
-import { JiraProjectLoader } from "./jira-project-loader";
 import { JiraSuggestionLoader } from "./jira-suggestions-loader";
 import { ComponentSink, ComponentSource } from "./helper";
 import { Suggestion } from "@/model/suggestion";
-import { Project } from "@/model/project";
 import { Issue } from "@/model/issue";
 import { Events } from "@/model/event";
 
 type State = {
   issues: Issue[];
-  project?: Project;
   suggestion?: Suggestion;
 };
 
@@ -28,10 +25,6 @@ export const JiraLoader = (sources: JiraLoaderSources): JiraLoaderSinks => {
   const issueLoaderSinks = isolate(JiraIssueLoader, { HTTP: "issues" })({
     ...sources,
     events: sources.events.filter((v) => v.kind !== "GetSuggestionRequest"),
-  });
-  const projectLoaderSinks = isolate(JiraProjectLoader, { HTTP: "project" })({
-    ...sources,
-    events: sources.events.filter((v) => v.kind === "GetWholeDataRequest"),
   });
   const suggestionLoaderSinks = isolate(JiraSuggestionLoader, { HTTP: "suggestion" })({
     ...sources,
@@ -50,14 +43,6 @@ export const JiraLoader = (sources: JiraLoaderSources): JiraLoaderSinks => {
         return { ...prevState, issues };
       }
   );
-  const projectReducer$: Stream<Reducer<State>> = projectLoaderSinks.project.map(
-    (project): Reducer<State> =>
-      (prevState) => {
-        if (!prevState) return prevState;
-
-        return { ...prevState, project };
-      }
-  );
   const suggestionsReducer$: Stream<Reducer<State>> = suggestionLoaderSinks.suggestion.map(
     (suggestion): Reducer<State> =>
       (prevState) => {
@@ -68,7 +53,7 @@ export const JiraLoader = (sources: JiraLoaderSources): JiraLoaderSinks => {
   );
 
   return {
-    HTTP: xs.merge(issueLoaderSinks.HTTP, projectLoaderSinks.HTTP, suggestionLoaderSinks.HTTP),
-    state: xs.merge(initialReducer$, issuesReducer$, projectReducer$, suggestionsReducer$),
+    HTTP: xs.merge(issueLoaderSinks.HTTP, suggestionLoaderSinks.HTTP),
+    state: xs.merge(initialReducer$, issuesReducer$, suggestionsReducer$),
   };
 };

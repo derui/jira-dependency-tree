03413138d9d97726f20af0bd79f57a4bced021d1
derui <derutakayu@gmail.com>
2023-01-11 21:15:58 +0900

    test: add test case

--- src/components/organisms/sync-issue-button.test.tsx
+++ src/components/organisms/sync-issue-button.test.tsx
@@ -10,6 +10,9 @@ import { createStore } from "@/state/store";
 import { env } from "@/env";
 import { createDependencyRegistrar } from "@/util/dependency-registrar";
 import { Dependencies } from "@/dependencies";
+import { submitApiCredentialFulfilled, submitProjectKeyFulfilled } from "@/state/actions";
+import { projectFactory } from "@/model/project";
+import { Loading } from "@/type";
 
 test.afterEach(cleanup);
 
@@ -29,4 +32,53 @@ test.serial("initial state is disabled all", (t) => {
   const button = screen.getByTestId("button/button") as HTMLBaseElement;
 
   t.is(button.getAttribute("aria-disabled"), "true");
+});
+
+test.serial("do not disable if setup finished", (t) => {
+  const store = createStore(registrar);
+  store.dispatch(submitProjectKeyFulfilled(projectFactory({ key: "key", id: "id", name: "name" })));
+  store.dispatch(
+    submitApiCredentialFulfilled({
+      apiBaseUrl: "url",
+      apiKey: "key",
+      email: "email",
+      token: "token",
+      userDomain: "domain",
+    }),
+  );
+
+  render(
+    <Provider store={store}>
+      <SyncIssueButton />
+    </Provider>,
+  );
+
+  const button = screen.getByTestId("button/button") as HTMLBaseElement;
+  t.is(button.getAttribute("aria-disabled"), "false");
+});
+
+test.serial("dispatch action when click action", async (t) => {
+  const store = createStore(registrar);
+  store.dispatch(submitProjectKeyFulfilled(projectFactory({ key: "key", id: "id", name: "name" })));
+  store.dispatch(
+    submitApiCredentialFulfilled({
+      apiBaseUrl: "url",
+      apiKey: "key",
+      email: "email",
+      token: "token",
+      userDomain: "domain",
+    }),
+  );
+
+  render(
+    <Provider store={store}>
+      <SyncIssueButton />
+    </Provider>,
+  );
+
+  const button = screen.getByTestId("button/button") as HTMLBaseElement;
+
+  await userEvent.click(button);
+
+  t.is(store.getState().issues.loading, Loading.Loading);
 });

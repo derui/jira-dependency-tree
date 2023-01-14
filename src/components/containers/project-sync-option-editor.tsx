4d048b180e0a236bd9c10f66df6836771f62399a
derui <derutakayu@gmail.com>
2023-01-14 13:38:09 +0900

    refactor: re-arch search condition editor

---
+++ src/components/containers/project-sync-option-editor.tsx
@@ -0,0 +1,80 @@
+import React, { useRef, useState } from "react";
+import classNames from "classnames";
+import { Dialog } from "../atoms/dialog";
+import { BaseProps, classes, generateTestId } from "../helper";
+import { useAppSelector } from "../hooks";
+import { ProjectSyncOptionEditorForm } from "../organisms/project-sync-option-editor-form";
+import { SearchCondition } from "@/model/event";
+import { isSearchConditionEditable, selectSearchCondition } from "@/state/selectors/project";
+import { Rect } from "@/util/basic";
+
+export type Props = BaseProps;
+
+const Styles = {
+  root: classes("relative", "w-full", "flex", "items-center", "justify-center"),
+  opener: (opened: boolean, disabled: boolean) => {
+    return {
+      ...classes(
+        "inline-flex",
+        "px-3",
+        "py-1",
+        "border",
+        "bg-white",
+        "rounded",
+        "transition-colors",
+        "cursor-pointer",
+        "items-center",
+        "whitespace-nowrap",
+        "text-sm",
+      ),
+
+      ...(opened ? classes("text-white", "bg-secondary1-200", "border-secondary1-500") : classes("bg-white")),
+      ...(disabled
+        ? classes("text-lightgray", "border-lightgray")
+        : classes("border-gray", "hover:text-white", "hover:bg-secondary1-200", "hover:border-secondary1-500")),
+    };
+  },
+};
+
+const currentConditionName = (condition: SearchCondition | undefined) => {
+  if (condition?.epic) {
+    return condition.epic;
+  } else if (condition?.sprint) {
+    return condition.sprint.displayName;
+  }
+
+  return "Current Sprint";
+};
+
+export const ProjectSyncOptionEditor: React.FC<Props> = (props) => {
+  const gen = generateTestId(props.testid);
+  const [opened, setOpened] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+  const searchCondition = useAppSelector(selectSearchCondition());
+  const isEditable = useAppSelector(isSearchConditionEditable());
+  const disabled = !isEditable;
+
+  return (
+    <div ref={ref} className={classNames(Styles.root)}>
+      <button
+        className={classNames(Styles.opener(opened, disabled))}
+        disabled={disabled}
+        data-testid={gen("opener")}
+        onClick={() => {
+          setOpened(!opened);
+        }}
+      >
+        {currentConditionName(searchCondition)}
+      </button>
+
+      <Dialog
+        testid={gen("form-dialog")}
+        aligned='bottomLeft'
+        opened={opened}
+        parentRect={ref.current ? Rect.fromDOMRect(ref.current.getBoundingClientRect()) : undefined}
+      >
+        <ProjectSyncOptionEditorForm onClose={() => setOpened(false)} testid='sync-editor-form' />
+      </Dialog>
+    </div>
+  );
+};

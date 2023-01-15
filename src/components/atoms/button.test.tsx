f534ffe143bbccddc8e5c917943d8a8c57784a29
derui <derutakayu@gmail.com>
2023-01-15 14:04:35 +0900

    test: fix type error

--- src/components/atoms/button.test.tsx
+++ src/components/atoms/button.test.tsx
@@ -15,7 +15,7 @@ test.serial("should be able to render", (t) => {
 
   const element = screen.getByTestId<HTMLButtonElement>("button");
 
-  t.is(element.ariaDisabled, undefined);
+  t.is(element.getAttribute("aria-disabled"), "false");
   t.is(element.type, "button");
 });
 
@@ -24,7 +24,7 @@ test.serial("render submit button", (t) => {
 
   const element = screen.getByTestId<HTMLButtonElement>("button");
 
-  t.is(element.ariaDisabled, undefined);
+  t.is(element.getAttribute("aria-disabled"), "false");
   t.is(element.type, "submit");
 });
 

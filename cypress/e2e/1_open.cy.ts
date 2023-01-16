4fc19ee34a6c63040cdea782611356502afdfcec
derui <derutakayu@gmail.com>
2023-01-16 19:45:59 +0900

    test: apply new attributes

--- cypress/e2e/1_open.cy.ts
+++ cypress/e2e/1_open.cy.ts
@@ -4,17 +4,19 @@ describe("open", () => {
 
     cy.testid("sync-issue-button/button").should("have.attr", "disabled");
     cy.testid("user-configuration/opener").should("not.have.class", "opened");
-    cy.testid("user-configuration/marker").should("have.class", "--show");
-    cy.testid("project-information/marker").should("have.class", "--show");
+    cy.testid("user-configuration/marker").should("have.attr", "aria-hidden", "false");
+    cy.testid("project-information/marker").should("have.attr", "aria-hidden", "false");
     cy.testid("project-information/main").and("contain.text", "Click here");
     cy.testid("project-information/nameEditor").should("not.have.class", "--opened");
     cy.testid("zoom-slider/current-zoom").should("contain.text", "100%");
-    cy.testid("sync-option-editor/opener").should("contain.text", "Current Sprint").should("have.attr", "disabled");
+    cy.testid("project-sync-option-editor/opener")
+      .should("contain.text", "Current Sprint")
+      .should("have.attr", "disabled");
 
     // side toolbar
     cy.testid("side-toolbar/graph-layout").should("not.have.class", "--opened");
-    cy.testid("side-toolbar/horizontal").should("have.class", "--selected");
-    cy.testid("side-toolbar/vertical").should("not.have.class", "--selected");
+    cy.testid("side-toolbar/horizontal/icon").should("have.data", "active", true);
+    cy.testid("side-toolbar/vertical/icon").should("have.data", "active", false);
 
     // searcher
     cy.testid("issue-searcher/opener").should("be.visible");

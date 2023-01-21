d6e8f43551337e403474656505e6084361330d2d
derui <derutakayu@gmail.com>
2023-01-21 17:25:20 +0900

    feat: add utility to generate color

---
+++ src/util/color.ts
@@ -0,0 +1,15 @@
+/**
+ * return color generated from any string
+ */
+export const stringToColour = (str: string) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = "#";
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    colour += ("00" + value.toString(16)).slice(-2);
+  }
+  return colour;
+};

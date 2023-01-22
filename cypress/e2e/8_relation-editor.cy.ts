d73c17cb710601b97359e1dcccef69ff1de2af80
derui <derutakayu@gmail.com>
2023-01-22 15:01:33 +0900

    fix: add case

--- cypress/e2e/8_relation-editor.cy.ts
+++ cypress/e2e/8_relation-editor.cy.ts
@@ -31,4 +31,34 @@ describe("edit relation of issue", () => {
     cy.testid("relation-editor/inward-editor/issue").should("have.length", 0);
     cy.testid("relation-editor/outward-editor/issue/root").should("have.length", 1).and("contain.text", "TES-2");
   });
+
+  it("close panel", () => {
+    cy.visit("/");
+
+    cy.mockAPI({
+      "http://localhost:3000/load-issues": post(["relation-editor/issues"]),
+      "http://localhost:3000/load-project": post(["basic/project"]),
+      "http://localhost:3000/get-suggestions": post(["basic/suggestions"]),
+    });
+
+    // Input credentials
+    cy.testid("user-configuration/opener").click();
+    cy.testid("user-configuration/form/user-domain/input").type("domain").should("have.value", "domain");
+    cy.testid("user-configuration/form/email/input").type("email").should("have.value", "email");
+    cy.testid("user-configuration/form/token/input").type("token").should("have.value", "token");
+    cy.testid("user-configuration/form/submit/button").click();
+
+    // input project name
+    cy.testid("project-information/name").click();
+    cy.testid("project-information/key/input").type("KEY");
+    cy.testid("project-information/submit/icon").click();
+    cy.testid("sync-issue-button/root").click();
+
+    // select issue
+    cy.get('[data-issue-key="TES-1"]').click();
+
+    // close panel
+    cy.testid("relation-editor/close/button").click();
+    cy.testid("relation-editor/root").should("not.be.visible").and("have.attr", "aria-hidden", "true");
+  });
 });

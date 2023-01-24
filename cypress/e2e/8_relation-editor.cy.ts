3d4ec8da18d4e9d1568b719a795042d51424eca5
derui <derutakayu@gmail.com>
2023-01-24 23:18:12 +0900

    test: add case to add relation successfully

--- cypress/e2e/8_relation-editor.cy.ts
+++ cypress/e2e/8_relation-editor.cy.ts
@@ -165,7 +165,7 @@ describe("edit relation of issue", () => {
     cy.testid("relation-editor/outward-editor/issue/root-skeleton").should("have.length", 0);
   });
 
-  it.only("add relation from suggestion, and failed to add relation", () => {
+  it("add relation from suggestion, and failed to add relation", () => {
     cy.visit("/");
 
     cy.mockAPI({
@@ -210,5 +210,47 @@ describe("edit relation of issue", () => {
       // verify issue is not loading
       cy.testid("relation-editor/inward-editor/issue/root").should("have.length", 0);
     });
+  });
+
+  it("add relation from suggestion, and success it", () => {
+    cy.visit("/");
+
+    cy.mockAPI({
+      "http://localhost:3000/load-issues": post(["relation-editor/issues"]),
+      "http://localhost:3000/load-project": post(["basic/project"]),
+      "http://localhost:3000/get-suggestions": post(["basic/suggestions"]),
+      "http://localhost:3000/create-link": post(["relation-editor/create-link"]),
+    });
+
+    // Input credentials
+    cy.testid("user-configuration/opener").click();
+    cy.testid("user-configuration/form/user-domain/input").type("domain").should("have.value", "domain");
+    cy.testid("user-configuration/form/email/input").type("email").should("have.value", "email");
+    cy.testid("user-configuration/form/token/input").type("token").should("have.value", "token");
+    cy.testid("user-configuration/form/submit/button").click();
+
+    // input project name
+    cy.testid("project-information/name").click();
+    cy.testid("project-information/key/input").type("KEY");
+    cy.testid("project-information/submit/icon").click();
+    cy.testid("sync-issue-button/root").click();
+
+    // select issue
+    cy.get('[data-issue-key="TES-1"]').click();
+
+    // click append button and suggestion
+    cy.testid("relation-editor/inward-editor/appender/add-button/button").should("have.text", "Add").click();
+    cy.testid("relation-editor/inward-editor/appender/issue-term/input").should("have.value", "").type("TES");
+
+    // verify issue suggestion and create link
+    cy.testid("relation-editor/inward-editor/appender/suggestion-list/suggestion")
+      .should("have.length", 2)
+      .and("contain.text", "TES-2")
+      .and("contain.text", "TES-3")
+      .first()
+      .click();
+
+    // success and add relation
+    cy.testid("relation-editor/inward-editor/issue/root").should("have.length", 1).and("contain.text", "TES-2");
   });
 });

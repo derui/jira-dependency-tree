173f016bcd982d43159db530096e2980f338e3df
derui <derutakayu@gmail.com>
2023-02-04 13:38:01 +0900

    feat: add undirected graph

---
+++ src/depgraph/undirected.test.ts
@@ -0,0 +1,77 @@
+import { test, expect } from "vitest";
+import { emptyUndirectedGraph } from "./undirected";
+
+test("create empty graph", () => {
+  const graph = emptyUndirectedGraph();
+
+  expect(graph.edges).empty;
+  expect(graph.vertices).empty;
+});
+
+test("add vertices but no edges", () => {
+  const graph = emptyUndirectedGraph().addVertex("a").addVertex("b");
+
+  expect(graph.vertices).contains("a").and.contain("b");
+  expect(graph.edges).empty;
+});
+
+test("should error when vertex is null", () => {
+  expect(() => emptyUndirectedGraph().addVertex("")).toThrow();
+});
+
+test("add edge", () => {
+  const graph = emptyUndirectedGraph().addVertices(["a", "b"]).edge("a", "b");
+
+  expect(graph.vertices).contain("a").and.contain("b");
+  expect(graph.edges).toHaveLength(1);
+});
+
+test("edges should unique", () => {
+  const graph = emptyUndirectedGraph().addVertices(["a", "b", "c"]).edge("a", "b").edge("b", "a").edge("a", "c");
+
+  expect(graph.vertices).contain("a").and.contain("b").and.contain("c");
+  expect(graph.edges).toHaveLength(2);
+});
+
+test("throw error to add edge with vertices are not in graph", () => {
+  const graph = emptyUndirectedGraph().addVertices(["a", "b"]);
+
+  expect(() => graph.edge("", "b")).toThrow();
+  expect(() => graph.edge("", "")).toThrow();
+  expect(() => graph.edge("a", "")).toThrow();
+  expect(() => graph.edge("c", "b")).toThrow();
+  expect(() => graph.edge("b", "b")).toThrow();
+});
+
+test("get subgraph orphan vertex", () => {
+  const graph = emptyUndirectedGraph().addVertices(["a", "b"]).subgraphOf("a");
+
+  expect(graph.vertices).toEqual(["a"]);
+  expect(graph.edges).empty;
+});
+
+test("get subgraph related edges", () => {
+  const graph = emptyUndirectedGraph().addVertices(["a", "b", "c", "d"]).edge("a", "b").edge("b", "c").subgraphOf("c");
+
+  expect(graph.vertices).contain("a").and.contain("b").contain("c").and.lengthOf(3);
+  expect(graph.edges).toHaveLength(2);
+  expect(graph.edges).toContainEqual(["a", "b"]);
+  expect(graph.edges).toContainEqual(["b", "c"]);
+});
+
+test("get subgraph with other root", () => {
+  const graph = emptyUndirectedGraph()
+    .addVertices(["a", "b", "c", "d", "e", "f"])
+    .edge("a", "b")
+    .edge("b", "c")
+    .edge("e", "d")
+    .edge("a", "d")
+    .subgraphOf("b");
+
+  expect(graph.vertices).contain("a").and.contain("b").contain("c").contain("d").contain("e").and.lengthOf(5);
+  expect(graph.edges).toHaveLength(4);
+  expect(graph.edges).toContainEqual(["a", "b"]);
+  expect(graph.edges).toContainEqual(["b", "c"]);
+  expect(graph.edges).toContainEqual(["a", "d"]);
+  expect(graph.edges).toContainEqual(["d", "e"]);
+});
---
+++ src/depgraph/undirected.ts
@@ -0,0 +1,186 @@
+import { AdjacentMatrix, Edge, Vertex } from "./type";
+import { constraint } from "@/util/basic";
+
+/**
+ * An interface of undirected graph.
+ */
+export interface UndirectedGraph {
+  /**
+   * add a vertex into graph
+   */
+  addVertex(label: Vertex): UndirectedGraph;
+
+  /**
+   * add vertices into graph
+   */
+  addVertices(vertices: Vertex[]): UndirectedGraph;
+
+  /**
+   * get subgraph contains vertex.
+   */
+  subgraphOf(vertex: Vertex): UndirectedGraph;
+
+  /**
+   * add edge between vertices that are specified `a` and `b`
+   */
+  edge(a: Vertex, b: Vertex): UndirectedGraph;
+
+  readonly edges: Edge[];
+  readonly vertices: Vertex[];
+}
+
+const addVertex = (vertex: Vertex, original: Vertex[]): Vertex[] => {
+  constraint(vertex.trim().length > 0);
+
+  if (original.includes(vertex)) {
+    return original;
+  }
+
+  return original.concat([vertex]);
+};
+const addVertices = (original: Vertex[], newVertices: Vertex[]): Vertex[] => {
+  const _original = new Set(original);
+
+  for (const v of newVertices) {
+    _original.add(v);
+  }
+
+  return Array.from(_original);
+};
+
+const makeAdjacentMatrix = (edges: Edge[], vertices: string[]): AdjacentMatrix => {
+  const matrix = vertices.reduce<AdjacentMatrix>((accum, vertex) => {
+    accum[vertex] = new Set();
+    return accum;
+  }, {});
+
+  edges.forEach(([a, b]) => {
+    matrix[a].add(b);
+    matrix[b].add(a);
+  });
+
+  return matrix;
+};
+
+/**
+ * support function to check equality of edges
+ */
+const sameEdge = (a: Edge, b: Edge) => {
+  const [a1, a2] = a;
+  const [b1, b2] = b;
+
+  return (a1 === b1 && a2 === b2) || (a1 === b2 && a2 === b1);
+};
+
+const addEdge = (edges: Edge[], a: Vertex, b: Vertex): Edge[] => {
+  for (const [_a, _b] of edges) {
+    if ((_a === a && _b === b) || (_a === b && _b === a)) {
+      return edges;
+    }
+  }
+
+  return edges.concat([[a, b]]);
+};
+
+// undirected graph-version dfs
+const dfs = (mat: AdjacentMatrix, root: Vertex, work: (node: Vertex) => void) => {
+  const seen = new Set<Vertex>();
+
+  const recursiveDfs = (mat: AdjacentMatrix, node: Vertex) => {
+    seen.add(node);
+
+    work(node);
+
+    const nextNodes = mat[node] || new Set();
+
+    for (const nextNode of nextNodes) {
+      if (seen.has(nextNode)) {
+        continue;
+      }
+
+      recursiveDfs(mat, nextNode);
+    }
+  };
+
+  return recursiveDfs(mat, root);
+};
+
+const edge = (a: Vertex, b: Vertex): Edge => {
+  const array = [a, b].sort();
+
+  return [array[0], array[1]];
+};
+
+const makeConsistEdges = (edges: Edge[]) => {
+  const corrected: Array<Edge> = [];
+
+  for (const [a, b] of edges) {
+    if (corrected.some((edge) => sameEdge(edge, [a, b]))) {
+      continue;
+    }
+
+    corrected.push(edge(a, b));
+  }
+
+  return corrected;
+};
+
+const makeGraph = (vertices: Vertex[], edges: Edge[]): UndirectedGraph => {
+  const _edges = makeConsistEdges(edges);
+  const adjMatrix = makeAdjacentMatrix(edges, vertices);
+
+  return {
+    addVertex(label) {
+      return makeGraph(addVertex(label, vertices), edges);
+    },
+
+    addVertices(_vertices) {
+      return makeGraph(addVertices(vertices, _vertices), edges);
+    },
+
+    edge(a, b) {
+      constraint(vertices.includes(a));
+      constraint(vertices.includes(b));
+      constraint(a !== b);
+
+      return makeGraph(vertices, addEdge(edges, a, b));
+    },
+
+    subgraphOf(vertex) {
+      const _vertices = new Set<Vertex>();
+
+      dfs(adjMatrix, vertex, (v) => {
+        _vertices.add(v);
+      });
+
+      // collect related edges from edges
+      const relatedEdges = Array.from(_vertices).reduce<Edge[]>((accum, v) => {
+        const adjacents = adjMatrix[v];
+
+        for (const adjacent of adjacents) {
+          const _edge: Edge = [v, adjacent].sort() as Edge;
+          if (accum.some((edge) => sameEdge(edge, _edge))) {
+            continue;
+          }
+          accum.push(_edge);
+        }
+
+        return accum;
+      }, []);
+
+      return makeGraph(Array.from(_vertices), relatedEdges);
+    },
+
+    get edges() {
+      return Array.from(_edges);
+    },
+
+    get vertices() {
+      return Array.from(vertices);
+    },
+  };
+};
+
+export const emptyUndirectedGraph = () => {
+  return makeGraph([], []);
+};

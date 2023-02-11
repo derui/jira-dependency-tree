747ed3fdfe2a15718e9ef08607f7cb4c4f85d12a
derui <derutakayu@gmail.com>
2023-02-11 15:10:11 +0900

    test: add case for search

--- cypress/e2e/9_subtask.cy.ts
+++ cypress/e2e/9_subtask.cy.ts
@@ -65,4 +65,39 @@ describe("load issues", () => {
     cy.get(".graph-issue").should("be.visible").should("have.length", 3);
     cy.testid("side-toolbar/graph-unroller/icon").should("have.attr", "aria-disabled", "true");
   });
+
+  it("search only issues in projected issue", () => {
+    cy.visit("/");
+
+    cy.mockAPI({
+      "http://localhost:3000/load-issues": post(["subtask/issues"]),
+      "http://localhost:3000/load-project": post(["basic/project"]),
+    });
+
+    // Input credentials
+    cy.testid("user-configuration/opener").click();
+    cy.testid("user-configuration/form/user-domain").type("domain").should("have.value", "domain");
+    cy.testid("user-configuration/form/email").type("email").should("have.value", "email");
+    cy.testid("user-configuration/form/token").type("token").should("have.value", "token");
+    cy.testid("user-configuration/form/submit").click();
+
+    // input project name
+    cy.testid("project-information/name").click();
+    cy.testid("project-information/key").type("KEY");
+    cy.testid("project-information/submit").click();
+
+    cy.testid("sync-issue-button/root").click();
+
+    // display only subtasks in the issue that is clicked notification
+    cy.contains("have 1 sub issues").click();
+
+    // search only sub-issues in target
+    cy.testid("issue-searcher/opener").should("be.visible").click();
+    cy.testid("issue-searcher/input").should("be.visible").type("task{enter}");
+
+    cy.testid("issue-searcher/issue/root")
+      .should("be.visible")
+      .should("have.length", 1)
+      .should("contain.text", "TES-6");
+  });
 });

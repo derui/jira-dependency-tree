4194e5086fabbabd08ae2b3071a48ac45f954afe
derui <derutakayu@gmail.com>
2023-02-15 21:08:47 +0900

    refactor: split project information editor into standalone component

---
+++ src/components/molecules/project-information-editor.test.tsx
@@ -0,0 +1,58 @@
+import { test, expect, afterEach } from "vitest";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { ProjectInformationEditor } from "./project-information-editor";
+
+afterEach(cleanup);
+
+test("should be able to render", () => {
+  render(<ProjectInformationEditor onEndEdit={() => {}} />);
+
+  const key = screen.getByPlaceholderText("Project Key") as HTMLInputElement;
+
+  expect(key.value).toBe("");
+});
+
+test("set initial payload as initial value", () => {
+  render(<ProjectInformationEditor initialPayload={{ projectKey: "KEY" }} onEndEdit={() => {}} />);
+
+  const key = screen.getByPlaceholderText("Project Key") as HTMLInputElement;
+
+  expect(key.value).toBe("KEY");
+});
+
+test("get payload when typed", async () => {
+  expect.assertions(1);
+
+  render(
+    <ProjectInformationEditor
+      onEndEdit={(obj) => {
+        expect(obj?.projectKey).toBe("KEY");
+      }}
+    />,
+  );
+
+  const key = screen.getByPlaceholderText("Project Key") as HTMLInputElement;
+
+  await userEvent.type(key, "KEY");
+
+  await userEvent.click(screen.getByTestId("submit"));
+});
+
+test("do not send payload if canceled", async () => {
+  expect.assertions(1);
+
+  render(
+    <ProjectInformationEditor
+      onEndEdit={(obj) => {
+        expect(obj).toBeUndefined();
+      }}
+    />,
+  );
+
+  const token = screen.getByPlaceholderText("Project Key") as HTMLInputElement;
+
+  await userEvent.type(token, "KEY");
+
+  await userEvent.click(screen.getByTestId("cancel"));
+});
---
+++ src/components/molecules/project-information-editor.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+import { BaseProps, classes, generateTestId } from "../helper";
+import { Input } from "../atoms/input";
+import { Icon } from "../atoms/icon";
+import { filterEmptyString } from "@/util/basic";
+
+type Payload = { projectKey: string };
+
+export interface Props extends BaseProps {
+  initialPayload?: Payload;
+  onEndEdit: (event: Payload | undefined) => void;
+}
+
+const Styles = {
+  form: classes("flex", "flex-col", "pb-0", "pt-4"),
+
+  // editor styles
+  keyEditorButtonGroup: classes("bg-white", "flex", "justify-end", "mt-2"),
+  keyEditorButton: classes("first:ml-0", "last:mr-0", "mx-2", "cursor-pointer"),
+};
+
+const canSubmit = (obj: Partial<Payload>) => {
+  return filterEmptyString(obj.projectKey);
+};
+
+export const ProjectInformationEditor: React.FC<Props> = ({ initialPayload, onEndEdit, ...props }) => {
+  const gen = generateTestId(props.testid);
+  const [obj, setObj] = useState({ ...initialPayload });
+  const allowSubmit = canSubmit(obj);
+
+  const update = (key: keyof typeof obj) => {
+    return (v: string) => {
+      setObj({ ...obj, [key]: v });
+    };
+  };
+
+  const handleCancel = () => {
+    onEndEdit(undefined);
+  };
+
+  const handleSubmit = () => {
+    if (filterEmptyString(obj.projectKey)) {
+      onEndEdit({ projectKey: obj.projectKey });
+    }
+  };
+
+  return (
+    <form className={classNames(Styles.form)} method='dialog' data-testid={gen("dialog")} onSubmit={handleSubmit}>
+      <Input
+        focus={true}
+        value={obj.projectKey || ""}
+        placeholder='Project Key'
+        testid={gen("key")}
+        onInput={update("projectKey")}
+      />
+      <span className={classNames(Styles.keyEditorButtonGroup)}>
+        <span
+          role='button'
+          className={classNames(Styles.keyEditorButton)}
+          onClick={handleCancel}
+          data-testid={gen("cancel")}
+        >
+          <Icon type='circle-x' color='gray' size='m' />
+        </span>
+        <span
+          role='button'
+          aria-disabled={!allowSubmit}
+          className={classNames(Styles.keyEditorButton)}
+          onClick={handleSubmit}
+          data-testid={gen("submit")}
+        >
+          <Icon type='circle-check' size='m' color='complement' />
+        </span>
+      </span>
+    </form>
+  );
+};

78a4aba8fa2dd21b0bb019ce2ee961c128842484
derui <derutakayu@gmail.com>
2023-05-20 10:52:08 +0900

    fix: do not use deprecated method

--- src/mock-data.ts
+++ src/mock-data.ts
@@ -9,27 +9,27 @@ import { StatusCategory } from "./type";
  */
 export const randomProject = () => {
   return projectFactory({
-    id: faker.datatype.uuid(),
+    id: faker.string.uuid(),
     key: faker.word.verb(),
-    name: faker.name.jobTitle(),
+    name: faker.person.jobTitle(),
     issueTypes: [
       {
-        id: faker.datatype.uuid(),
-        name: faker.datatype.string(),
+        id: faker.string.uuid(),
+        name: faker.string.sample(),
         avatarUrl: faker.internet.url(),
       },
     ],
     statusCategories: [
       {
-        id: faker.datatype.uuid(),
-        name: faker.datatype.string(),
+        id: faker.string.uuid(),
+        name: faker.string.sample(),
         colorName: faker.color.human(),
       },
     ],
     statuses: [
       {
-        id: faker.datatype.uuid(),
-        name: faker.datatype.string(),
+        id: faker.string.uuid(),
+        name: faker.string.sample(),
         statusCategory: StatusCategory.IN_PROGRESS,
       },
     ],
@@ -54,13 +54,13 @@ export const randomCredential = (): ApiCredential => {
  */
 export const randomIssue = (issue: Partial<Issue> = {}): Issue => {
   return {
-    key: issue.key ?? faker.datatype.string(),
+    key: issue.key ?? faker.string.sample(),
     summary: issue.summary ?? faker.lorem.sentence(),
     description: issue.description ?? faker.company.catchPhrase(),
     relations: issue.relations ?? [],
     selfUrl: issue.selfUrl ?? faker.internet.url(),
-    statusId: issue.statusId ?? faker.datatype.uuid(),
-    typeId: issue.typeId ?? faker.datatype.uuid(),
+    statusId: issue.statusId ?? faker.string.uuid(),
+    typeId: issue.typeId ?? faker.string.uuid(),
     parentIssue: issue.parentIssue,
     subIssues: issue.subIssues ?? [],
   };

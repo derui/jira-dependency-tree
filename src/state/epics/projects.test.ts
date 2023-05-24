05f2f1294983d945e441c790eff0780c828194eb
derui <derutakayu@gmail.com>
2023-05-25 07:47:40 +0900

    test: add test

--- src/state/epics/projects.test.ts
+++ src/state/epics/projects.test.ts
@@ -1,12 +1,79 @@
-import { test, expect } from "vitest";
+import { test, expect, describe } from "vitest";
+import { TestScheduler } from "rxjs/testing";
+import { StateObservable } from "redux-observable";
+import { NEVER } from "rxjs";
 import { createDependencyRegistrar } from "../../util/dependency-registrar";
 import { Dependencies } from "../../dependencies";
+import { createPureStore } from "../store";
+import { projects, submitApiCredentialFulfilled } from "../actions";
 import * as epic from "./projects";
+import { randomCredential } from "@/mock-data";
 
-const registrar = createDependencyRegistrar<Dependencies>();
-const env = {
-  apiBaseUrl: "http://base.url",
-  apiKey: "key",
-};
+describe("load projects", () => {
+  test("get error if credential was not setupeped", async () => {
+    const registrar = createDependencyRegistrar<Dependencies>();
 
-test("test epic", async (t) => {});
+    const testScheduler = new TestScheduler((a, b) => expect(a).toEqual(b));
+    const store = createPureStore();
+
+    testScheduler.run(({ hot, expectObservable: expect }) => {
+      const epics = epic.projectsEpic(registrar);
+
+      const action$ = hot("-a", {
+        a: projects.loadProjects(),
+      });
+      const state$ = new StateObservable(NEVER, store.getState());
+
+      const ret$ = epics.loadProjects(action$, state$, null);
+
+      expect(ret$).toBe("-a", {
+        a: projects.loadProjectsError({ reason: "Can not run with no credential" }),
+      });
+    });
+  });
+
+  test("get project", () => {
+    const registrar = createDependencyRegistrar<Dependencies>();
+
+    const testScheduler = new TestScheduler((a, b) => expect(a).toEqual(b));
+    const store = createPureStore();
+    store.dispatch(submitApiCredentialFulfilled(randomCredential()));
+
+    testScheduler.run(({ hot, cold, expectObservable: expect }) => {
+      const response = {
+        values: [
+          {
+            id: "foo",
+            key: "bar",
+            name: "name",
+          },
+          {
+            id: "id2",
+            key: "baz",
+            name: "second",
+          },
+        ],
+      };
+      registrar.register("postJSON", () => {
+        return cold("--a", { a: response });
+      });
+      const epics = epic.projectsEpic(registrar);
+
+      const action$ = hot("-a", {
+        a: projects.loadProjects(),
+      });
+      const state$ = new StateObservable(NEVER, store.getState());
+
+      const ret$ = epics.loadProjects(action$, state$, null);
+
+      expect(ret$).toBe("---a", {
+        a: projects.loadProjectsSucceeded({
+          projects: [
+            { id: "foo", key: "bar", name: "name" },
+            { id: "id2", key: "baz", name: "second" },
+          ],
+        }),
+      });
+    });
+  });
+});
--- src/state/epics/projects.ts
+++ src/state/epics/projects.ts
@@ -1,13 +1,58 @@
 import { Epic } from "redux-observable";
 import type { Action } from "@reduxjs/toolkit";
+import { catchError, filter, map, of, startWith, switchMap } from "rxjs";
 import type { RootState } from "../store";
+import { projects } from "../actions";
+import { SimpleProject } from "../models/simple-project";
 import type { Dependencies } from "@/dependencies";
 import { DependencyRegistrar } from "@/util/dependency-registrar";
 
-type Epics = "";
+type Epics = "loadProjects";
 
 export const projectsEpic = (
   registrar: DependencyRegistrar<Dependencies>,
 ): Record<Epics, Epic<Action, Action, RootState>> => ({
-  // implement epics
+  loadProjects: (action$, state$) =>
+    action$.pipe(
+      filter(projects.loadProjects.match),
+      switchMap(() => {
+        const credential = state$.value.apiCredential.credential;
+
+        if (!credential) {
+          return of(projects.loadProjectsError({ reason: "Can not run with no credential" }));
+        }
+
+        return registrar
+          .resolve("postJSON")({
+            url: `${credential.apiBaseUrl}/load-projects`,
+            headers: {
+              "x-api-key": credential.apiKey,
+            },
+            body: {
+              authorization: {
+                jira_token: credential.token,
+                email: credential.email,
+                user_domain: credential.userDomain,
+              },
+            },
+          })
+          .pipe(
+            map((response) => mapResponse(response as { values: unknown[] })),
+            map((ret) => projects.loadProjectsSucceeded({ projects: ret })),
+          );
+      }),
+      catchError((e, source) => {
+        console.error(e);
+
+        return source.pipe(startWith(projects.loadProjectsError({ reason: "failed with exception" })));
+      }),
+    ),
 });
+
+const mapResponse = function mapResponse(response: { values: unknown[] }): SimpleProject[] {
+  return response.values.map((res) => {
+    const { id, key, name } = res as { id: string; key: string; name: string };
+
+    return { id, key, name };
+  });
+};

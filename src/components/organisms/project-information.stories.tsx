def769180371565f10daf3f9b8407403878463af
derui <derutakayu@gmail.com>
2023-05-27 08:50:05 +0900

    fix: add story for project information

--- src/components/organisms/project-information.stories.tsx
+++ src/components/organisms/project-information.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Provider } from "react-redux";
 import { ProjectInformation } from "./project-information";
 import { createPureStore } from "@/state/store";
+import { projects, submitProjectKey } from "@/state/actions";
 
 const meta = {
   title: "Organisms/Project Information",
@@ -16,6 +17,28 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   render() {
     const store = createPureStore();
+    store.dispatch(
+      projects.loadProjectsSucceeded({
+        projects: [
+          { id: "id", key: "key", name: "name" },
+          { id: "2", key: "key2", name: "name2" },
+        ],
+      }),
+    );
+
+    return (
+      <Provider store={store}>
+        <div id="dialog-root" />
+        <ProjectInformation />
+      </Provider>
+    );
+  },
+};
+
+export const Loading: Story = {
+  render() {
+    const store = createPureStore();
+    store.dispatch(submitProjectKey("key"));
 
     return (
       <Provider store={store}>

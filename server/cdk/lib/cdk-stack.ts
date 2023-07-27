7ac73b3894b98e588097158e55882282c22843cb
derui <derutakayu@gmail.com>
2023-07-27 21:59:13 +0900

    fix: empower timeout to 1 minute

--- server/cdk/lib/cdk-stack.ts
+++ server/cdk/lib/cdk-stack.ts
@@ -10,6 +10,7 @@ export class CdkStack extends cdk.Stack {
     const jiraIssueHandlerLambda = new lambda.DockerImageFunction(this, "jiraIssueLoader", {
       functionName: "jira-issue-loader",
       code: lambda.DockerImageCode.fromImageAsset("../"),
+      timeout: cdk.Duration.minutes(1),
     });
 
     const restApi = new apigw.LambdaRestApi(this, "Endpoint", {

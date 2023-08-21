41d71cac4d44f00e70066ad85b985aa8960d95f8
derui <derutakayu@gmail.com>
2023-08-21 20:41:27 +0900

    feat: add search issue to apis

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,6 +1,7 @@
 import * as createRelation from "./create-relation";
 import * as removeRelation from "./remove-relation";
 import * as getIssues from "./get-issues";
+import * as searchIssues from "./search-issues";
 
 /**
  * An entry point of APIs
@@ -9,4 +10,5 @@ export const Apis = {
   createRelation,
   removeRelation,
   getIssues,
+  searchIssues,
 };

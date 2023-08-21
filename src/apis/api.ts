204146a8b40c5b11da7143f8a93a2ce4742cbe69
derui <derutakayu@gmail.com>
2023-08-21 20:05:20 +0900

    feat: integrate apis into single entrypoint

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,4 +1,12 @@
+import * as createRelation from "./create-relation";
+import * as removeRelation from "./remove-relation";
+import * as getIssues from "./get-issues";
+
 /**
  * An entry point of APIs
  */
-export const Apis = {};
+export const Apis = {
+  createRelation,
+  removeRelation,
+  getIssues,
+};
---
+++ src/apis/remove-relation.ts
@@ -0,0 +1,26 @@
+import { ApiCredential } from "@/model/event";
+import { IssueRelationId } from "@/type";
+
+/**
+ * execute to get a issues
+ */
+export const call = async function call(apiCredential: ApiCredential, relationId: IssueRelationId): Promise<void> {
+  const body = {
+    authorization: {
+      jira_token: apiCredential.token,
+      email: apiCredential.email,
+      user_domain: apiCredential.userDomain,
+    },
+    id: relationId,
+  };
+
+  await fetch(`${apiCredential.apiBaseUrl}/delete-link`, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "content-type": "application/json",
+      "x-api-key": apiCredential.apiKey,
+    },
+    body: JSON.stringify(body),
+  });
+};

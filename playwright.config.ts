994edc139c7d1e59b417a8146712f027e2bcf5e5
derui <derutakayu@gmail.com>
2023-09-03 16:22:01 +0900

    fix: do not restrict worker count

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "dot",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */

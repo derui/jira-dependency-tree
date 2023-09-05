76b3bed36010214085b32a71f6b3d258da92e710
derui <derutakayu@gmail.com>
2023-09-05 21:42:51 +0900

    fix: add simple color function to get invert color

--- src/util/color.ts
+++ src/util/color.ts
@@ -9,7 +9,21 @@ export const stringToColour = (str: string) => {
   let colour = "#";
   for (let i = 0; i < 3; i++) {
     const value = (hash >> (i * 8)) & 0xff;
-    colour += ("00" + value.toString(16)).slice(-2);
+    colour += value.toString(16).padStart(2, "0");
   }
   return colour;
+};
+
+const color_regexp = /[a-fA-F0-9]{6}/;
+
+export const colorToInvertColor = function colorToInvertColor(color: string) {
+  if (color.length !== 7 || color[0] !== "#" || !color.slice(1).match(color_regexp)) {
+    throw new Error(`invalid color spec ${color}`);
+  }
+
+  const r = 255 - Number.parseInt(color.slice(1, 3), 16);
+  const g = 255 - Number.parseInt(color.slice(3, 5), 16);
+  const b = 255 - Number.parseInt(color.slice(5, 7), 16);
+
+  return `#${r.toString(16).padStart(2, "0")}${g.toString(16).padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
 };

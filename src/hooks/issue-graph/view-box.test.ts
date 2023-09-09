69c7f9fcbd3d50225bed1a78e222ae6d6c0494b0
derui <derutakayu@gmail.com>
2023-09-09 10:34:10 +0900

    feat: add hook to get view box

---
+++ src/hooks/issue-graph/view-box.test.ts
@@ -0,0 +1,64 @@
+import { test, expect } from "vitest";
+import { renderHook } from "@testing-library/react";
+import { useViewBox } from "./view-box";
+import { Rect } from "@/utils/basic";
+
+/**
+ * utility function
+ */
+const rect = function rect(width: number, height: number) {
+  return new Rect({ left: 0, top: 0, right: width, bottom: height });
+};
+
+test("initial view box", () => {
+  const { result } = renderHook(() => useViewBox());
+
+  expect(result.current.state).toEqual({
+    viewBox: [0, 0, 0, 0],
+  });
+});
+
+test("calculate view box", () => {
+  // Arrange
+  const { result, rerender } = renderHook(() => useViewBox());
+
+  // Act
+  result.current.resize(rect(100, 150));
+  rerender();
+
+  // Assert
+  expect(result.current.state).toEqual({
+    viewBox: [0, 0, 100, 150],
+  });
+});
+
+test("calculate view box with pan", () => {
+  // Arrange
+  const { result, rerender } = renderHook(() => useViewBox());
+
+  // Act
+  result.current.resize(rect(100, 150));
+  result.current.movePan({ x: 15, y: 25 });
+  rerender();
+
+  // Assert
+  expect(result.current.state).toEqual({
+    viewBox: [7.5, 12.5, 100, 150],
+  });
+});
+
+test("calculate view box with zoom", () => {
+  // Arrange
+  const { result, rerender } = renderHook(() => useViewBox());
+
+  // Act
+  result.current.resize(rect(100, 150));
+  result.current.movePan({ x: -100, y: -100 });
+  result.current.zoomOut(10);
+  rerender();
+
+  // Assert
+  expect(result.current.state).toEqual({
+    viewBox: [-100, -125, 200, 300],
+  });
+});
---
+++ src/hooks/issue-graph/view-box.ts
@@ -0,0 +1,78 @@
+import { useCallback, useMemo, useState } from "react";
+import { Position } from "@/type";
+import { Rect } from "@/utils/basic";
+
+type Result = {
+  /**
+   * change pan of view box
+   */
+  movePan: (delta: Position) => void;
+
+  /**
+   * zoom in more.
+   */
+  zoomIn: (ratioDiff: number) => void;
+
+  /**
+   * zoom out more
+   */
+  zoomOut: (ratioDiff: number) => void;
+
+  /**
+   * resize svg canvas size
+   */
+  resize: (size: Rect) => void;
+
+  state: {
+    /**
+     * calculated view box for <svg>
+     */
+    viewBox: number[];
+  };
+};
+
+export const useViewBox = function useViewBox(): Result {
+  const [zoom, setZoom] = useState(100.0);
+  const [pan, setPan] = useState<Position>({ x: 0, y: 0 });
+  const [size, setSize] = useState<Rect>(Rect.empty());
+  const viewBox = useMemo<Result["state"]["viewBox"]>(() => {
+    const scale = 100 / zoom;
+    const zoomedWidth = size.width * scale;
+    const zoomedHeight = size.height * scale;
+    const centerX = pan.x + size.width / 2;
+    const centerY = pan.y + size.height / 2;
+
+    const newMinX = centerX - zoomedWidth / 2;
+    const newMinY = centerY - zoomedHeight / 2;
+
+    return [newMinX, newMinY, zoomedWidth, zoomedHeight];
+  }, [pan, size, zoom]);
+
+  const movePan = useCallback<Result["movePan"]>((delta) => {
+    setPan((pan) => {
+      return { x: pan.x + (delta.x * (100 / zoom)) / 2, y: pan.y + (delta.y * (100 / zoom)) / 2 };
+    });
+  }, []);
+
+  const zoomIn = useCallback<Result["zoomIn"]>((delta) => {
+    setZoom((zoom) => {
+      const zoomScale = delta * 5 * (zoom / 100);
+
+      return Math.min(200, zoom + zoomScale);
+    });
+  }, []);
+
+  const zoomOut = useCallback<Result["zoomOut"]>((delta) => {
+    setZoom((zoom) => {
+      const zoomScale = delta * 5 * (zoom / 100);
+
+      return Math.max(1, zoom - zoomScale);
+    });
+  }, []);
+
+  const resize = useCallback<Result["resize"]>((size) => {
+    setSize(size);
+  }, []);
+
+  return { movePan, resize, zoomIn, zoomOut, state: { viewBox: viewBox } };
+};

e8e123ab20acfeec0c32f566b82df7d27ab53afc
derui <derutakayu@gmail.com>
2023-09-23 09:34:31 +0900

    feat: add layout grid

---
+++ src/hooks/issue-graph/_layout-grid.ts
@@ -0,0 +1,113 @@
+import { produce } from "immer";
+import { DirectedGraph } from "@/libs/depgraph/main";
+import { filterUndefined } from "@/utils/basic";
+
+type VertexWithLevel = {
+  readonly vertex: string;
+  readonly level: number;
+};
+
+const empty: unique symbol = Symbol("empty");
+const vertex: unique symbol = Symbol("vertex");
+
+type Empty = { kind: typeof empty };
+type Vertex = { kind: typeof vertex; vertex: string };
+type Placement = Empty | Vertex;
+
+export type T = {
+  /**
+   * place vertices to grid. return new instance of [T]
+   */
+  place(from: VertexWithLevel, to: VertexWithLevel): T;
+
+  /**
+   * get vertex of position specified
+   */
+  position(col: number, row: number): string | undefined;
+
+  /**
+   * layouted grid
+   */
+  readonly layout: ReadonlyArray<ReadonlyArray<Placement>>;
+};
+
+export const isEmpty = function isEmpty(obj: Placement): obj is Empty {
+  return obj.kind == empty;
+};
+
+/**
+ * return new layout grid.
+ */
+const newLayoutGrid = function newLayoutGrid(layout: T["layout"]): T {
+  const layoutedVertices = new Set(
+    layout
+      .map((v) => {
+        return v.map((v) => {
+          switch (v.kind) {
+            case empty:
+              return;
+            case vertex:
+              return v.vertex;
+          }
+        });
+      })
+      .flat()
+      .filter(filterUndefined),
+  );
+
+  return {
+    position(col, row) {
+      const v = layout[col][row];
+
+      switch (v.kind) {
+        case empty:
+          return;
+        case vertex:
+          return v.vertex;
+      }
+    },
+
+    place(from, to) {
+      const { vertex: fromV, level: fromL } = from;
+      const { vertex: toV, level: toL } = to;
+
+      if (fromV === toV || fromL >= toL) {
+        throw new Error(`Invalid place call: from=${JSON.stringify(from)}, to=${JSON.stringify(to)}`);
+      }
+
+      const edited = produce(layout, (draft) => {
+        const levelDiff = toL - fromL - 1;
+
+        // Fill in the empty if the levels are not different by one
+        for (let l = 1; l <= levelDiff; l++) {
+          draft[fromL + l].push({ kind: empty });
+        }
+
+        if (!layoutedVertices.has(fromV)) {
+          draft[fromL].push({ kind: vertex, vertex: fromV });
+          layoutedVertices.add(fromV);
+        }
+
+        if (!layoutedVertices.has(toV)) {
+          draft[toL].push({ kind: vertex, vertex: toV });
+          layoutedVertices.add(toV);
+        }
+      });
+
+      return newLayoutGrid(edited);
+    },
+
+    layout,
+  } satisfies T;
+};
+
+/**
+ * get new layout grid from graph
+ */
+export const make = function make(graph: DirectedGraph): T {
+  const largestDepth = graph.maxDepth();
+
+  const layout = Array.from(Array(largestDepth), () => []);
+
+  return newLayoutGrid(layout);
+};
